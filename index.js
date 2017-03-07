62ec5a82b858d1c8ea1d6ecd68d2313ab75d7400
1ambda <1amb4a@gmail.com>
2017-03-07 20:37:31 +0900

    feat: Support delay config

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {
   SpellBase,
   SpellResult,
@@ -27,11 +10,8 @@ export default class EchoSpell extends SpellBase {
   }
 
   interpret(paragraphText, config) {
-    let repeat = 1
 
-    try {
-      repeat = parseInt(config.repeat)
-    } catch(error) { /** use default value */ }
+    const { repeat, delay, } = this.getConfig(config)
 
     let text = ''
 
@@ -39,6 +19,24 @@ export default class EchoSpell extends SpellBase {
       text += `${paragraphText}\n`
     }
 
-    return new SpellResult(text)
+    const p = new Promise(resolve => {
+      setTimeout(() => {
+        resolve(text)
+      }, delay)
+    })
+
+    return new SpellResult(p)
+  }
+
+  getConfig(config) {
+    let repeat = 1
+    let delay = 1000
+
+    try {
+      repeat = parseInt(config.repeat)
+      delay = parseInt(config.delay)
+    } catch(error) { /** use default value */ }
+
+    return { repeat, delay, }
   }
 }
